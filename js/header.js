a23a6f3297fc05dc334412e659ef0bf80d1f990b
“Viktor <viktor.zvarych@gmail.com>
2022-12-13 00:31:27 +0200

    added timer of war and count of killed orks

--- js/header.js
+++ js/header.js
@@ -6,12 +6,16 @@
   const navigationsBody = document.querySelector(".nav-wrapper");
   const clockContainer = document.querySelector(".header-clock");
   const timerUntilChristmas = document.querySelector(".header-christmas");
+  const timerDaysOfWar = document.querySelector(".days-of-war");
+  const countOfKilledOrks = document.querySelector(".count-of-killed-orks");
   iconBurger.addEventListener("click", function (e) {
     document.body.classList.toggle("lock");
     iconBurger.classList.toggle("active");
     navigationsBody.classList.toggle("active");
     clockContainer.classList.toggle("hidden");
     timerUntilChristmas.classList.toggle("hidden");
+    timerDaysOfWar.classList.toggle("hidden");
+    countOfKilledOrks.classList.toggle("hidden");
   });
 
   const navLinks = document.querySelectorAll(".nav-menu__item");
@@ -68,10 +72,49 @@
     document.querySelector(".header-christmas-hours").innerHTML =
       hours + " hours";
     document.querySelector(".header-christmas-minutes").innerHTML =
-      minutes + " minutes";
+      minutes + " min.";
     document.querySelector(".header-christmas-seconds").innerHTML =
-      seconds + " seconds";
+      seconds + " sec.";
     setTimeout(calculateChristmasCountdown, 1000);
   }
   calculateChristmasCountdown();
+
+  /* TIMER OF WAR */
+  let totalDaysOfWar;
+  function calculateDaysOfWar() {
+    const dateOfWarStarted = "2022-02-24T00:05:00Z";
+
+    const now = new Date();
+    const datefromAPITimeStamp = new Date(dateOfWarStarted).getTime();
+    const nowTimeStamp = now.getTime();
+
+    const microSecondsDiff = Math.abs(datefromAPITimeStamp - nowTimeStamp);
+
+    // Math.round is used instead of Math.floor to account for certain DST cases
+    // Number of milliseconds per day =
+    //   24 hrs/day * 60 minutes/hour * 60 seconds/minute * 1000 ms/second
+    totalDaysOfWar = Math.round(microSecondsDiff / (1000 * 60 * 60 * 24));
+    return totalDaysOfWar;
+  }
+  calculateDaysOfWar();
+  console.log(totalDaysOfWar);
+
+  function showTotalDaysOfWar() {
+    timerDaysOfWar.textContent =
+      totalDaysOfWar + " days since full-scale invasion";
+  }
+  showTotalDaysOfWar();
+
+  /*COUNT OF KILLED ORKS*/
+  async function showCountOfKilledOrks() {
+    const response = await fetch(
+      "https://russianwarship.rip/api/v1/statistics/latest"
+    );
+    const warStats = await response.json();
+
+    const numberOfKilledOrks = warStats.data.stats["personnel_units"];
+    countOfKilledOrks.textContent =
+      numberOfKilledOrks + " orks have been killed";
+  }
+  showCountOfKilledOrks();
 })();
