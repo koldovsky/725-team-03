63a194831ac14055328b9b5a7a05e33630ba3764
Lesyk03 <novosyadlo.oles@gmail.com>
2022-12-12 11:38:29 +0000

    code

--- js/works.js
+++ js/works.js
@@ -9,6 +9,32 @@
 
   function renderSlide(){
       const slideContainer = document.querySelector('.latest-work_carousel_slide-container');
-      slideContainer.innerHTML = currentSlideInd;
+      slideContainer.innerHTML = slides[currentSlideInd];
+      if (window.innerWidth > 880 ){
+        const secondSlideInd = currentSlideInd + 1 >= slides.length ? 0 : currentSlideInd +1;
+        slideContainer.innerHTML += slides[secondSlideInd];
+
+      }
   }
+
+  function nextSlide(){
+    currentSlideInd++;
+    if (currentSlideInd >= slides.length) currentSlideInd = 0;
+    renderSlide();
+  }
+  function prevSlide(){
+    currentSlideInd--;
+    if (currentSlideInd < 0) currentSlideInd = slides.length-1;
+    renderSlide();
+  }
+  setInterval(nextSlide, 3000);
+
+  renderSlide();
+
+  const nextButton = document.querySelector('.latest-work_carousel_btn-next')
+  nextButton.addEventListener('click', nextSlide);
+ 
+  const prevButton = document.querySelector('.latest-work_carousel_btn-prev')
+  prevButton.addEventListener('click', prevSlide);
+
 })();
